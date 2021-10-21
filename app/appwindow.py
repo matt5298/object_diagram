465298a24d67afac404a4a96d44f0dd31ff4a937
Matthew Jackson <matt5298@gmail.com>
2021-10-21 07:15:42 -0600

    v0.05 in progress modifying how pointer object works with tags

--- app/appwindow.py
+++ app/appwindow.py
@@ -1,14 +1,74 @@
 # The main window for the application
 
+# idea
+    # have the ability to visually build a command
+    # A data object list
+        # has list of data
+        # displays the data
+        # can scroll the data if it's off the screen
+        # can search the data by typing in text (regex support?). requires a data entry area
+        # can select one or more items by clicking on them.
+        # drop the object on another and the selected items are copied or moved to it.
+        
+    # have an data object that can display, search, select tags
+    # have an data object that can display, search, select tags
+
 # Additional abilities needed from pointer
     # ability to trigger an activity.  For example a TimeoutError
     # Maybe the objects that are created and are children can have different reactions to the double click Event
     # Send the double click event to the group if pointer is in group mode.  Then the invividual objects react based upon if they have a doubel clicked function to call or not
     # Send the double click event to the individual item if pointer is in id mode. still will only react if it has the double clicked function
 
+# To do
+    # display all tags from all objects on the canvas in a compound object
+
+# to do
+    # to create a compound object
+    # have _id_background: the is also the lowest member because all other objects will be .tag_raise(<this group tag>, <background id>)
+    # have the count of member objects
+    # _add_object(): to add an object and put it in the correct display number
+    # tag_object(): to add a tag to all objects in the object
+    # remove_object(): to remove the object tag from the object.
+    # to readjust all elements position in the display list use .tag_raise(<this group tag>, <background id>)
+    # add new text item to the object
+    # have object able to be hidden and shown.  Use tags to designate hidden and shown items.
+    # have app tags that are prefixed with _
+    # have a right click function display a context sensitive menu
+
+
+    # detection of collisions happens with the background object
+    # detection of overlaps can happen with the background object, and/or with the component objects
+    
+
+# v0.05
+    # currently I have the dragging and dropping working in the following manner
+        # the mode is id or sourceNameTag
+        # added to _drag_data, sourceNameTag, sourceIndex (the location in the display list)
+        # in pointer.select() get the sourceIndex for the lowest item in the id or sourceNameTag
+        # in pointer._drop_def() return the item to it's location which is below the item currently in sourceIndex of display list
+        # have not implemented in pointer._drop_move()
+            # bugs in the pointer._drop_move for pointer id move mode.  haven't tested pointer group move mode.
+
+    #Todo
+        # new attribute tags to identify object attributes
+        # _background: for the background of the object.  This can only be dragged in group mode with the Name tag
+        # _item: for an item that is a member of the object.  This is dragged by it's Item tag
+
+
+    # modify pointer code
+        # in object mode get the Name tag and move the object
+        ## in item move mode get the Item tag and then drop on the Name tag if there is one
+        # for _background drag in tag mode using the Name tag
+        # for _item drag in tag mode using the Item tag
+        # adding a tag to the selected object _source
+        # adding a tag to the target _target
+        # then delete the sources Name tag and add to it the target's Name tag
+        # then remove the source and target tags
+
 # v0.04 pointer_mode object implemented
     # this encapsulated all the drag and drop functionality for the different pointer modes.
-    # have only implemented the group and id modes.  Need to now implement group move and id move.
+    # have implemented group, id, group move, id move modes.
+
 
 # v0.03 drag and drop functionality
 #   additional pointer mode: dnd move
@@ -50,7 +110,15 @@ class Pointer_Mode:
         # which identifier out of _drag_data will be used
         # id or tagName. used as keyname to retrieve correct value from _drag_data dictionary
         self._mode = "id"
-        self._drag_data = {"x": 0, "y": 0, "id": None, "tagName": None}
+        # sourceNameTag is the name tag that is for the object clicked on
+        # targetNameTag is the name tag of the drop target
+        # displayListStart is the 
+        self._drag_data = {"x": 0, "y": 0, "id":None, "sourceNameTag":None, "targetNameTag":None, "sourceIndex":None}
+        # tag that will be used to mark the item(s) being dragged
+        self._drag_source = '_Source'
+        # marks the item that is the target in a drag and drop
+        self._drag_target = '_Target'
+
         # need to setup the public methods
         # always present the functions: select, move, drop
         # if they need to be modified then overwrite in an inherited
@@ -60,16 +128,28 @@ class Pointer_Mode:
 
     def select(self, event):
         """Begining drag of a group"""
-        # record the item and its location
+        # record its location and tag it
         # get the object id and tagName
-        self._drag_data['id'] = self.myCanvas.find_closest(event.x, event.y)[0]
-        self._drag_data['tagName'] = self._getObjectNameTag(self._drag_data['id'])
+        id = self.myCanvas.find_closest(event.x, event.y)[0]
+        self._drag_data['id'] = id 
+        self._drag_data['sourceNameTag'] = self._getObjectNameTag(self._drag_data['id'])
         self._drag_data["x"] = event.x
         self._drag_data["y"] = event.y
-        #raise it to the topmost in the canvas
+        #get the location in the display list of the current item based on _mode
+        # this is the index of the item in the list of all items.
+        
+        # get array of items in canvas with the tag or id
+        items = self.myCanvas.find_all()
+        # the first item in the find_withtag(tag or id) returned array is the lowest one in the display list.  
+        sourceLowItem = self.myCanvas.find_withtag(self._drag_data[self._mode])[0]
+        # get it's index in the items list
+        self._drag_data['sourceIndex'] = items.index(sourceLowItem)
+        #print('{}, {}'.format(self._drag_data["sourceIndex"],items))
+        
+        #raise selected item to the topmost in the canvas
         self.myCanvas.tag_raise(self._drag_data[self._mode], 'all')
 
-        print('Pointer mode: {}'.format(self._mode))
+        #print('Pointer mode: {}'.format(self._mode))
 
     def move(self, event):
         """Handle dragging of a group"""
@@ -84,7 +164,18 @@ class Pointer_Mode:
 
     def _drop_def(self, event):
         """End drag of an object"""
-        # no action taken, default drop
+        # return the item to it's place in the display
+        # acts on the tags of the selected item.
+        print('Before replacing: {}'.format(self.myCanvas.find_all()))
+        
+        # get all the items from which we can get the item at the index of sourceIndex
+        items = self.myCanvas.find_all()
+        #lower the current item to below the item in the location of sourceIndex
+        #this will place the item to where it was befor.  If _mode = id then only the one item moves
+        #if _mode=sourceNameTag then all items with that tag will be moved and maintain their relative locations.
+        self.myCanvas.tag_lower(self._drag_data[self._mode],items[self._drag_data['sourceIndex']])
+        print('After replacing: {}'.format(self.myCanvas.find_all()))
+
         # reset the drag information
         self._drag_data["id"] = None
         self._drag_data["item"] = None
@@ -135,7 +226,7 @@ class Pointer_Mode:
         # add the new Name tag to the source object to group it with the object dropped on
         # need to use the 'source' tag because the Name tag that's in the source variable no longer exists.
         self.myCanvas.addtag_withtag( tagTarget, 'source')
-        print ('Adding to source {}, target tag {}'.format( source, tagTarget))
+        print ('Adding to source {}, target tag {}'.format( 'source', tagTarget))
         print(self.myCanvas.gettags(self._drag_data[self._mode]))
         # reset the drag information
         self._drag_data["id"] = None
@@ -143,6 +234,24 @@ class Pointer_Mode:
         self._drag_data["x"] = 0
         self._drag_data["y"] = 0
 
+    def _getObjectTagStartWith(self, id, startWith):
+            # this is to return the tag that startWith
+            # assuming looking for a tag that only one value exists
+            # so returning the first string in the returned aray
+            #print ('the id: {}'.format(id))
+            tags = self.myCanvas.gettags(id)
+            # find the tag that startsWith by find()==0
+            tagName = [tg for tg in tags if tg.find(startWith)==0]
+            #print ('The id and name tag are: {},{}'.format(id, tagName[0],))
+            return tagName[0]
+
+    def _setObjectTag(self, targetTag, newTag):
+        # on the targetTag add the newTag
+        self.myCanvas.addtag_withtag(targetTag, newTag)      
+    
+    def _deleteObjectTag(self, targetTag, deleteTag):
+        # on the targetTag delete the deleteTag
+        self.myCanvas.dtag(targetTag, deleteTag)
 
     def _getObjectNameTag(self, id):
             #print ('the id: {}'.format(id))
@@ -158,48 +267,43 @@ class Pointer_Mode:
 
 class Pointer_mode_group(Pointer_Mode):
     def __init__(self, myCanvas):
+        # call the parent
+        super().__init__(myCanvas=myCanvas)
+        # override properties
+        self._mode = 'sourceNameTag'
         self.name = "Pointer Group"
-        self.myCanvas = myCanvas
-        # which identifier out of _drag_data will be used
-        # id or tagName
-        self._mode = "tagName"
-        self._drag_data = {"x": 0, "y": 0, "id": None, "tagName": None}
+        #point drop method to needed internal method
         self.drop = self._drop_def
 
 class Pointer_mode_group_move(Pointer_Mode):
     def __init__(self, myCanvas):
+        # call the parent
+        super().__init__(myCanvas=myCanvas)
+        # override properties
+        self._mode = 'sourceNameTag'
         self.name = "Pointer Group Move"
-        self.myCanvas = myCanvas        
-        # which identifier out of _drag_data will be used
-        # id or tagName (for group)
-        self._mode = "tagName"
-        self._drag_data = {"x": 0, "y": 0, "id": None, "tagName": None}
-        # set to the correct method
+        #point drop method to needed internal method
         self.drop = self._drop_move
-
 
 class Pointer_mode_id(Pointer_Mode):
     def __init__(self, myCanvas):
+        # call the parent
+        super().__init__(myCanvas=myCanvas)
+        # override properties
+        self._mode = 'id'
         self.name = "Pointer Id"
-        self.myCanvas = myCanvas        
-        # which identifier out of _drag_data will be used
-        # id or tagName
-        self._mode = "id"
-        self._drag_data = {"x": 0, "y": 0, "id": None, "tagName": None}
+        #point drop method to needed internal method
         self.drop = self._drop_def
 
 class Pointer_mode_id_move(Pointer_Mode):
     def __init__(self, myCanvas):
+        # call the parent
+        super().__init__(myCanvas=myCanvas)
+        # override properties
+        self._mode = 'id'
         self.name = "Pointer Id Move"
-        self.myCanvas = myCanvas        
-        # which identifier out of _drag_data will be used
-        # id or tagName
-        self._mode = "id"
-        self._drag_data = {"x": 0, "y": 0, "id": None, "tagName": None}
-        # set to the correct method
+        #point drop method to needed internal method
         self.drop = self._drop_move
-
-
 
 class AppWindow():
     def __init__(self):
@@ -208,6 +312,8 @@ class AppWindow():
 
         # for testing using a global variable to increment each time object is created
         self.i = 0
+        # using a global variable to increment each time an object element is created
+        self.j = 0
 
         # configure layout of window
         self.window.rowconfigure(0,minsize=800, weight=1)
@@ -215,7 +321,7 @@ class AppWindow():
 
         #setting up window controls
         #canvas object to draw on
-        self.myCanvas = tk.Canvas(self.window, bg="white")
+        self.myCanvas = tk.Canvas(self.window, bg="light grey")
         coord = 10, 10, 300, 300
         # container for buttons
         self.fr_buttons = tk.Frame(self.window)
@@ -281,6 +387,7 @@ class AppWindow():
         x=10
         y=10
         color = "grey"
+        # this is the background of the object.  It's synonymous with the object.
         canvas.create_rectangle(
                 x,
                 y,
@@ -288,11 +395,12 @@ class AppWindow():
                 y + height,
                 outline=color,
                 fill=color,
-                tags=("object",tagName),
+                tags=("object",'_background',tagName),
             )
         self.setBindings()
 
         # create text with the same tag
+        # these are items of the object so they have the Name tag and the Item tag with unique
         textValues = [tagName, 'one','two','what ever','Hello World!!']
         # is text going to be left justified or center justified
         justify = 'left'
@@ -307,6 +415,11 @@ class AppWindow():
         for row in range (0,5,1):
             rowHeight = 15
             txtY = (rowHeight * row) + y
+            #create unique item tag for this item
+            tagItem = 'Item' + str(self.j)
+            # increment the j counter for the next item
+            self.j += 1
+
             # format the font of the entries 
             if row == 0:
                 fnt = ('Helvetica 15 bold')
@@ -318,7 +431,7 @@ class AppWindow():
                 text = textValues[row],
                 fill = "white",
                 font = fnt,
-                tags=("object",tagName),
+                tags=("object",'_item',tagName, tagItem),
                 anchor = anchor,
         )
 
